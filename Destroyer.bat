07fc6736be6abec132c30a48dcc83ee9ec21e48b
Telefony i komputery <phonecollector2025@gmail.com>
2025-02-25 16:30:11 +0100

    Destroyer.bat

--- Destroyer.bat
+++ Destroyer.bat
@@ -1,7 +1,7 @@
 @echo off
 :1
 color a
-echo What have you just executed will damage your files. Make sure you're using safe environment before executing this script. Do you want to continue running this script resulting in unusable computer? (answer in only yes/no)
+echo What have you just executed will damage your files. Make sure you're using safe environment before executing this script. Do you want to continue running this script resulting in an unusable computer? (answer in only yes/no)
 set /p input=
 if /i %input%==yes goto delete
 if /i %input%==no goto save
