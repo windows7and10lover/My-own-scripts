f367925a35016c9c3f9ece741fab400484c03043
Telefony i komputery <phonecollector2025@gmail.com>
2025-02-27 17:49:23 +0100

    Destroyer.bat

--- Destroyer.bat
+++ Destroyer.bat
@@ -16,3 +16,15 @@ exit
 :save
 pause
 exit
+if !osNumber! lss 52 (
+	cls
+
+	title Unsupported Operating System Detected
+	color 0a
+
+	echo Sorry :( , but this script cannot be launched on an older Operating System. 
+	echo Please try it again on at least Windows XP x64/Vista.
+	echo.
+	
+	pause
+	exit
