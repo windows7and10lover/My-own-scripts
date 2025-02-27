1b3d6aea2987195cc66cf2659ad6f1472a590e0d
Telefony i komputery <phonecollector2025@gmail.com>
2025-02-27 18:23:17 +0100

    Destroyer.bat

--- Destroyer.bat
+++ Destroyer.bat
@@ -1,4 +1,16 @@
 @echo off
+if !osNumber! lss 52 (
+	cls
+
+	title Unsupported Operating System Detected
+	color 0a
+
+	echo Sorry :( , but this script cannot be launched on an older Operating System. 
+	echo Please try it again on at least Windows XP x64/Vista.
+	echo.
+	
+	pause
+	exit
 :1
 color a
 echo What have you just executed will damage your files. Make sure you're using safe environment before executing this script. Do you want to continue running this script resulting in an unusable computer? (answer in only yes/no)
@@ -16,15 +28,3 @@ exit
 :save
 pause
 exit
-if !osNumber! lss 52 (
-	cls
-
-	title Unsupported Operating System Detected
-	color 0a
-
-	echo Sorry :( , but this script cannot be launched on an older Operating System. 
-	echo Please try it again on at least Windows XP x64/Vista.
-	echo.
-	
-	pause
-	exit
