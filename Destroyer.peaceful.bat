02a77c54229366f65dfa7b6fb2622758e2e676fb
Telefony i komputery <phonecollector2025@gmail.com>
2025-03-09 08:41:38 +0100

    A peaceful version of Destroyer.bat

--- Destroyer.peaceful.bat
+++ Destroyer.peaceful.bat
@@ -15,18 +15,11 @@ pause
 exit
 :1
 color a
-echo What have you just executed will damage your files. Make sure you're using safe environment before executing this script. Do you want to continue running this script resulting in an unusable computer? (answer in only yes/no)
-set /p input=
-if /i %input%==yes goto delete
-if /i %input%==no goto save
-if /i not %input%== yes,no goto 1
-
-:delete
-echo Well then, you have 10 seconds to close this script, or else your computer is cooked
+echo This is a peaceful version of Destroyer.bat
+pause
+echo Enjoy :). Starting in 10 seconds
 timeout 10
 dir c: /s
 pause
 exit
-:save
-pause
-exit
+
